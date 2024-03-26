5c1ed6500b91d13c31a829291cb007749860187e
Ari Han <ahrihan@gmail.com>
2024-03-26 18:58:55 +0900

    Create 홀짝 구분하기.swift

---
+++ Lv.0/홀짝 구분하기.swift
@@ -0,0 +1,16 @@
+//
+//  홀짝 구분하기.swift
+//  Lv.0
+//
+//  Created by Ari Han on 3/26/24.
+//
+
+import Foundation
+
+let a = Int(readLine()!)!
+
+if a % 2 == 0 {
+    print("\(a) is even")
+} else {
+    print("\(a) is odd")
+}
