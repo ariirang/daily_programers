553ec00ab19a0d9dd3a431877d5fe56e752880ef
Ari Han <ahrihan@gmail.com>
2024-03-27 21:27:17 +0900

    Create 문자 리스트를 문자열로 변환하기.swift

---
+++ Lv.0/문자 리스트를 문자열로 변환하기.swift
@@ -0,0 +1,18 @@
+//
+//  문자 리스트를 문자열로 변환하기.swift
+//  Lv.0
+//
+//  Created by Ari Han on 3/27/24.
+//
+
+import Foundation
+
+func solution(_ arr:[String]) -> String {
+    
+    var str = ""
+    
+    for i in arr {
+        str.append(i)
+    }
+    return str
+}
