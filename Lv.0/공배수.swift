d05af965842113378c395aadecf56917d4f25c36
Ari Han <ahrihan@gmail.com>
2024-03-28 20:07:50 +0900

    Create 공배수.swift

---
+++ Lv.0/공배수.swift
@@ -0,0 +1,17 @@
+//
+//  공배수.swift
+//  Lv.0
+//
+//  Created by Ari Han on 3/28/24.
+//
+
+import Foundation
+
+func solution(_ number:Int, _ n:Int, _ m:Int) -> Int {
+    
+    if number % n == 0 && number % m == 0 {
+        return 1
+    } else {
+        return 0
+    }
+}
